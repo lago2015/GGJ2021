496ba57755e2c1e402ef586e48223f5fac9b8cdb
Stephen Roebuck <shroebuck@gmail.com>
2021-01-27 15:39:18 -0800

    Gameplay Camera Setup.
    - Added Initial Split Screen Setup.
    - Script for flipping between Split Screen and Finale.

---
+++ Duplicate/Assets/Camera/Scripts/CameraChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+
+    public bool swap;
+
+    public bool swapback;
+
+    public Animator animators;
+    public List<Cinemachine.CinemachineVirtualCamera> regularVCameras;
+    public List<Cinemachine.CinemachineVirtualCamera> finalVCameras;
+    public List<GameObject> followCameras;
+    public GameObject finalCamera;
+
+    void Update() {
+        if(swap) {
+            swap = false;
+            animators.SetTrigger("Full");
+            finalVCameras.ForEach(n => n.Priority = 101);
+            regularVCameras.ForEach(n => n.Priority = 99);
+        }
+
+        if(swapback) {
+            swapback = false;
+            finalCamera.SetActive(false);
+            regularVCameras.ForEach(n => n.Priority = 101);
+            finalVCameras.ForEach(n => n.Priority = 99);
+        }
+    }
+
+    public void Final() {
+        finalCamera.SetActive(true);
+    }
+}
