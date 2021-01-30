ccd524ca983931961e1195347bb834ef5e3139f2
Seanbud <seanbud@users.noreply.github.com>
2021-01-29 19:58:57 -0500

    Built out ProgressBar component

--- Duplicate/Assets/ProgressBar-UI/ProgressBar.cs
+++ Duplicate/Assets/ProgressBar-UI/ProgressBar.cs
@@ -1,8 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
-    // TODO @Sean
+    [Required] public Slider SliderL;
+    [Required] public Slider SliderR;
+    public static ProgressBar _instance;
+
+    private void Awake()
+    {
+        if (_instance == null) _instance = this;
+    }
+
+    /// <param name="value">zero to one</param>
+    [Button]
+    public void UpdateSliders(float value)
+    {
+        SliderL.value = value;
+        SliderR.value = value;
+    }
 }
